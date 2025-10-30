e0336b1eb50b8a44e73401c7cc3c37d5000a1047
Doongiho <happyluck0426@gmail.com>
2025-10-30 17:29:09 +0900

    feat(api): 알림 읽음 처리 기능 구현

--- libs/api/notification.ts
+++ libs/api/notification.ts
@@ -6,3 +6,10 @@ export const fetchNotifications = async (type: 'chore' | 'notice') => {
   const res = await api.get(endpoint)
   return res.data
 }
+
+// 알림 읽음 처리
+export const patchReadNotification = async (type: 'chore' | 'notice', id: number) => {
+  const endpoint = type === 'chore' ? `/notifications/chores/${id}` : `/notifications/notices/${id}`
+  const res = await api.patch(endpoint)
+  return res.data
+}
