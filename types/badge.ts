ff1b83e26e8f63be77f3ca82a444e4989d8a7cea
minheewon <lkmj12qw@gmail.com>
2025-11-02 00:59:50 +0900

    feat(badge): 미션 및 마이뱃지 api 연동

--- types/badge.ts
+++ types/badge.ts
@@ -1,5 +1,11 @@
-export type ResponseTopBadge = {
-  type: string // ex) 'LARGE_J', 'EXPERT_KITCHEN', 'SEED_CHORE'
+export type ResponseBadge = {
+  id: number
+  badgeType: string
+  badgeTitle: string
+  description: string
   acquired: boolean
+  currentCount: number
+  requiredCount: number
   remainingCount: number
+  imageBadgeUrl: string
 }
