a830e4c62d1b04e41ce8e937109be28aa6fa6d33
전준호 <76806109+joonho0410@users.noreply.github.com>
2026-01-27 17:58:21 +0900

    Detail, Delete endpoint 수정, delete ApplyToAfter 제거 (#193)
    * Detail, Delete endpoint 수정, delete ApplyToAfter 제거
    
    * chore: retrigger vercel preview
    
    ---------
    
    Co-authored-by: Junho jeon <junhjeon@Junhoui-MacBookAir.local>

--- libs/api/chore/deleteChore.ts
+++ libs/api/chore/deleteChore.ts
@@ -1,10 +1,8 @@
 import { api } from '../axios'
 import { CHORE_ENDPOINTS } from '../endpoints'
 
-export async function deleteChore(choreInstanceId: number, applyToAfter: boolean) {
-  const { data } = await api.delete(CHORE_ENDPOINTS.DELETE(choreInstanceId), {
-    params: { applyToAfter },
-  })
+export async function deleteChore(choreInstanceId: number) {
+  const { data } = await api.delete(CHORE_ENDPOINTS.DELETE(choreInstanceId))
 
   return data
 }
